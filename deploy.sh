7a12487c7e4e637fcbd8fbccbdf012899b830feb
Nicolae Cojuhari <nicojuhari@gmail.com>
2021-12-18 22:26:03 +0100

    amends in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -13,8 +13,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:nicojuhari/math-practice.git main:gh-pages
+git subtree push --prefix dist origin gh-pages
 
 rm -rf dist
-
-cd -

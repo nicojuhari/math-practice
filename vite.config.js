55a69eee53f5564310f6c5d037bb4a818b5e9d53
Nicolae Cojuhari <nicojuhari@gmail.com>
2021-12-18 22:19:56 +0100

    deploy

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import Components from 'unplugin-vue-components/vite'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: '/math-practice/',
-  plugins: [vue(), Components({ /* options */ })]
-})
